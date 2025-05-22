a3340af03c90640bb4a0bceb121dcca4e51e01bb
winnie <147368347+flumbles@users.noreply.github.com>
2025-05-22 16:28:13 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -2,11 +2,21 @@
 
 from ezmm import Image
 
-from defame.fact_checker import FactChecker
+from defame.fact_checker import FactChecker, RateLimitedFactChecker
 
-fact_checker = FactChecker(llm="gpt_4o")
+fact_checker = RateLimitedFactChecker(
+  llm="gpt_4o", 
+  max_result_len=1200,
+  max_iterations=3,
+  tools_config={
+      "searcher": {
+          "limit_per_search": 4,
+          "max_result_len": 2000
+      },
+      "geolocator": None  # default
+  })
 claim = ["The image",
-         Image("in/example/sahara.webp"),
-         "shows the Sahara in 2023 covered with snow!"]
-report, _ = fact_checker.verify_claim(claim)
+         Image("in/example/Myanmar.png"),
+         "shows the beautiful fields in Myanmar"]
+report, _ = fact_checker.verify_claim_with_rate_limit(claim)
 report.save_to("out/fact-check")

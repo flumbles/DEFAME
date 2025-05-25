21b03bdc43fafd896dc78fe5d2f1bde36305ca1f
winnie <147368347+flumbles@users.noreply.github.com>
2025-05-25 18:04:29 +0800

    Update run_claims.py

--- scripts/run_claims.py
+++ scripts/run_claims.py
@@ -3,13 +3,20 @@
 from ezmm import Image
 
 from defame.fact_checker import FactChecker, RateLimitedFactChecker
-
+'''
 fact_checker = RateLimitedFactChecker(llm="gpt_4o", tools_config={
         "searcher": None,  # default
         "geolocator": None  # default
-    })
-claim = ["The image",
-         Image("in/example/Sahara.webp"),
+    })'''
+
+fact_checker = FactChecker(llm="llava_next")
+'''claim = ["The image",
+         Image("in/example/sahara.webp"),
          "shows the Sahara snowing!"]
-report, _ = fact_checker.verify_claim_with_rate_limit(claim)
-report.save_to("out/fact-check")
+report, _ = fact_checker.verify_claim(claim)
+report.save_to("out/fact-check")'''
+
+claim = ["The Sahara desert gets snow every winter."]
+report, meta = fact_checker.verify_claim(claim)
+print(f"✅ Result: {report.verdict}")
+print(f"📝 Justification: {report.justification}")
